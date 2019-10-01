dc66c3be7c0db0bccded22ecb3ed1d3e31d27e06
ngodi <albertngodi@gmail.com>
2019-10-01 19:29:08 +0100

    html inputs created

--- src/js/script.js
+++ src/js/script.js
@@ -8,6 +8,8 @@ function Book(title, author, numPages, read) {
 }
 
 function addBookToLibrary() {
+  let title = document.querySelector("#title").value;
+  let author = document.querySelector("#title").value;
   let newBook = new Book(title, author, numPages, read)
   myLibrary.push(newBook);
 }
